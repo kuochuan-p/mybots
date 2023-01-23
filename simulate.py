7a14317cd04dadd364c6f788acda79625f3ad295
kuochuan-p <kponzio@uvm.edu>
2023-01-23 17:55:39 -0500

    added generate.py

--- simulate.py
+++ simulate.py
@@ -1,8 +1,8 @@
 import pybullet as p
 import time as t
 
-physicsClient = p.connect(p.GUI)
-for i in range(0,1000):
+physicsClient = p.connect(p.DIRECT)
+for i in range(0,100):
     p.stepSimulation()
     t.sleep(1/100)
     print(i)
