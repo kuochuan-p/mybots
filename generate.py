d934a8433f01b23b6c4a3fafab5ec80032f337ed
kuochuan-p <kponzio@uvm.edu>
2023-01-29 22:56:25 -0500

    joints start

--- generate.py
+++ generate.py
@@ -1,1 +1,18 @@
 import pyrosim.pyrosim as pyrosim
+
+length, width, height = 1,1,1
+x,y,z = 0,0,0.5
+pyrosim.Start_SDF("boxes.sdf")
+for i in range(0,5):
+    for j in range(0,5):
+        for k in range(0,10):
+            pyrosim.Send_Cube(name="Box"+str(i)+str(j)+str(k), pos=[x,y,z] , size=[length,width,height])
+            z+=1
+            length, width, height = .9*length, .9*width, .9*height
+        z=.5
+        length, width, height = 1,1,1
+        x+=1
+    x=0
+    y+=1
+
+pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,8 +1,16 @@
 import pybullet as p
+import pybullet_data
 import time as t
 
-physicsClient = p.connect(p.DIRECT)
-for i in range(0,100):
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0,0,-9.8)
+
+p.loadSDF("boxes.sdf")
+planeId = p.loadURDF("plane.urdf")
+
+for i in range(0,1000):
     p.stepSimulation()
     t.sleep(1/100)
     print(i)
